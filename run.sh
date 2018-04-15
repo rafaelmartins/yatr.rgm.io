941fed05846f32cce9801876a365aca56dac9f05
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-04-15 17:05:23 +0200

    initial commit. added run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+I() {
+    if [[ "x${TRAVIS}" != "xtrue" ]]; then
+        echo "Not running on Travis-CI"
+        exit 1
+    fi
+
+    # travis only supports amd64
+    local arch="amd64"
+    local os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+    local index="$(curl --silent https://distfiles.rgm.io/yatr/LATEST/)"
+    local archive="$(echo "${index}" | sed -n "s/.*\(yatr-${os}-${arch}-.*\)\.sha512.*/\1/p")"
+    local folder="$(echo "${index}" | sed -n "s/.*\(yatr-${os}-${arch}-.*\)\.tar.*\.sha512.*/\1/p")"
+    local p="$(echo "${folder}" | sed "s/yatr-${os}-${arch}-\(.*\)/yatr-\1/")"
+
+    curl --fail --silent --output "${archive}" "https://distfiles.rgm.io/yatr/${p}/${archive}"
+    curl --fail --silent --output "${archive}.sha512" "https://distfiles.rgm.io/yatr/${p}/${archive}.sha512"
+
+    sha512sum --check --status "${archive}.sha512"
+    tar --extract --file="${archive}"
+
+    exec "${folder}/yatr"
+}
+
+I
